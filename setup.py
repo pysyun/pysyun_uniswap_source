b00e2db68134cc7c70354c4613cd94272e5687f6
ImmortalTapok <119121359+ImmortalTapok@users.noreply.github.com>
2023-09-12 21:15:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pysyun_uniswap_source',
-    version='1.0',
+    version='1.1',
     author='Immortal Tapok',
     author_email='immortaltapok@lineardev.net',
     py_modules=['pysyun_uniswap_source.uniswap_source', 'pysyun_uniswap_source.abi.uniswap_abi'],

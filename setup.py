c639b88c31a8326ef1f64fba752be8066237c1c1
pysyun <48061047+pysyun@users.noreply.github.com>
2023-09-21 00:55:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='1.1.3',
     author='Immortal Tapok',
     author_email='immortaltapok@lineardev.net',
-    py_modules=['pysyun_uniswap_source.uniswap_source', 'pysyun_uniswap_source.abi.uniswap_abi'],
+    py_modules=['pysyun_uniswap_source.uniswap_source', 'pysyun_uniswap_source.abi.uniswap_abi', 'pysyun_uniswap_source.abi.uniswap_factory_abi'],
     install_requires=['web3', 'requests', 'python-dotenv', 'tqdm']
 )

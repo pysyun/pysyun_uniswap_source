dbbcdef33ae14102e1076b66f9ad988d4e74b213
pysyun <48061047+pysyun@users.noreply.github.com>
2023-09-21 00:55:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pysyun_uniswap_source',
-    version='1.1.3',
+    version='1.1.4',
     author='Immortal Tapok',
     author_email='immortaltapok@lineardev.net',
     py_modules=['pysyun_uniswap_source.uniswap_source', 'pysyun_uniswap_source.abi.uniswap_abi', 'pysyun_uniswap_source.abi.uniswap_factory_abi'],
